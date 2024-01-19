5886a8d938089719ce9aa1a5e147aa319eadaccb
Nikitos~ <nik228625@gmail.com>
2024-01-19 19:54:11 +0200

    first commit

---
+++ src/Main.java
@@ -0,0 +1,59 @@
+public class Main {
+    public static void main(String[] args) {
+        int a1 = 1;
+        if (a1 == 0) {
+            System.out.println("Верно");
+        } else {
+            System.out.println("Неверно");
+        }
+
+        int a2 = 1;
+        if (a2 > 0) {
+            System.out.println("Верно");
+        } else {
+            System.out.println("Неверно");
+        }
+
+        int a3 = 1;
+        if (a3 < 0) {
+            System.out.println("Верно");
+        } else {
+            System.out.println("Неверно");
+        }
+
+        int a4 = 1;
+        if (a4 >= 0) {
+            System.out.println("Верно");
+        } else {
+            System.out.println("Неверно");
+        }
+
+        int a5 = 1;
+        if (a5 <= 0) {
+            System.out.println("Верно");
+        } else {
+            System.out.println("Неверно");
+        }
+
+        int a6 = 1;
+        if (a6 != 0) {
+            System.out.println("Верно");
+        } else {
+            System.out.println("Неверно");
+        }
+
+        String a7 = "test";
+        if (a7.equals("test")) {
+            System.out.println("Верно");
+        } else {
+            System.out.println("Неверно");
+        }
+
+        String a8 = "1";
+        if (a8.equals("1")) {
+            System.out.println("Верно");
+        } else {
+            System.out.println("Неверно");
+        }
+    }
+}
